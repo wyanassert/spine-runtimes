d307f9c0dbf953205b706ac1df1d288c861aac59
wyanwan <wyanwan@tencent.com>
2023-05-25 19:44:02 +0800

    点击 切换动画

--- spine-cocos2dx/example/Classes/NTFExample.cpp
+++ spine-cocos2dx/example/Classes/NTFExample.cpp
@@ -7,9 +7,11 @@
 
 #include "NTFExample.h"
 #include <spine/Extension.h>
+#include <iostream>
 
 USING_NS_CC;
 using namespace spine;
+using namespace std;
 
 PowInterpolation ntfPow2(2);
 PowOutInterpolation ntfPowOut2(2);
@@ -24,7 +26,7 @@ Scene* NTFExample::scene () {
 bool NTFExample::init () {
     if (!LayerColor::initWithColor(Color4B(128, 128, 128, 255))) return false;
 
-    skeletonNode = SkeletonAnimation::createWithJsonFile("NFT_1.json", "NFT_1.atlas", 1.f);
+    skeletonNode = SkeletonAnimation::createWithJsonFile("NFT_1.json", "NFT_1.atlas", 0.7f);
     skeletonNode->setAnimation(0, "room_guitar_right", true);
 //    skeletonNode->addAnimation(1, "gun-grab", false, 2);
     skeletonNode->setTwoColorTint(true);
@@ -34,18 +36,52 @@ bool NTFExample::init () {
     
     skeletonNode->setVertexEffect(&ntfEffect);
 
-    skeletonNode->setPosition(Vec2(_contentSize.width / 2, 20));
+    skeletonNode->setPosition(Vec2(_contentSize.width / 2, 150));
     addChild(skeletonNode);
 
     scheduleUpdate();
     
     EventListenerTouchOneByOne* listener = EventListenerTouchOneByOne::create();
     listener->onTouchBegan = [this] (Touch* touch, cocos2d::Event* event) -> bool {
-//        if (!skeletonNode->getDebugBonesEnabled()) {
-//            skeletonNode->setDebugBonesEnabled(true);
-//            skeletonNode->setDebugMeshesEnabled(true);
-//        } else if (skeletonNode->getTimeScale() == 1)
-//            skeletonNode->setTimeScale(0.3f);
+        static int index = 0;
+        std::string animations[] = {
+            "avatar",
+            "beach_sitted",
+            "enlarge",
+            "enter_pool",
+            "gym_meditate",
+            "hold_both",
+            "hold_left",
+            "hold_right",
+            "mj_five",
+            "mj_four",
+            "mj_one",
+            "mj_three",
+            "mj_two",
+            "normal",
+            "normal_picked",
+            "normal_sit",
+            "normal_sitted",
+            "normal_sitted_listen",
+            "normal_stand",
+            "normal_stand_listen",
+            "normal_stand_scratch",
+            "player_sit",
+            "room_drum",
+            "room_guitar_low",
+            "room_guitar_right",
+            "room_piano",
+            "sitted_lazysofa",
+            "sitted_motorcycle",
+            "sitted_tent",
+            "stand_pool",
+            "test"
+        };
+        index %= 31;
+        std::string currentAni = animations[index];
+        cout << currentAni << endl;
+        index ++;
+        skeletonNode->setAnimation(0, currentAni, true);
         
         return true;
     };
